d912244e6744202d4541012488eb37ad7cb5ca71
hey-oise <oise.giwa.okugbe@gmail.com>
2026-06-10 21:55:57 +0100

    init

--- app/login/page.jsx
+++ app/login/page.jsx
@@ -4,11 +4,21 @@ import Link from "next/link";
 export default function LogIn() {
   return (
     <div>
-      <Link className='fixed top-5 left-5 text-blue-400 hover:underline' href={'/'}>Go back home</Link>
-      <Link className='fixed top-5 right-5 text-blue-400 hover:underline' href={'/signup'}>sign up</Link>
-    <div className="h-screen flex justify-center items-center">
-      <SignIn/>
+      <Link
+        className="fixed top-5 left-5 text-blue-400 hover:underline"
+        href={"/"}
+      >
+        Go back home
+      </Link>
+      <Link
+        className="fixed top-5 right-5 text-blue-400 hover:underline"
+        href={"/signup"}
+      >
+        sign up
+      </Link>
+      <div className="h-screen flex justify-center items-center w-full p-10 m-auto">
+        <SignIn />
       </div>
     </div>
-  )
+  );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -3,13 +3,30 @@ import Link from "next/link";
 export default function App() {
   return (
     <div className="h-screen flex justify-center items-center flex-col gap-5 text-center">
-      <h1 className="text-2xl text-blue-400"> Save, Organize, and Reuse Your Code Snippets </h1>
-      <h1 className="text-1xl text-center w-md text-blue-200">Stop searching through old projects, Slack messages, and Notes app. Keep your favorite snippets in one place. Copy them back instantly when you need them.</h1>
+      <h1 className="text-2xl text-blue-400">
+        {" "}
+        Save, Organize, and Reuse Your Code Snippets{" "}
+      </h1>
+      <h2 className="text-1xl max-w-2xl text-center text-blue-200">
+        Stop searching through old projects, Slack messages, and Notes app. Keep
+        your favorite snippets in one place. Copy them back instantly when you
+        need them.
+      </h2>
       <div className="flex flex-row gap-2">
-        <Link className="border border-gray-600 p-2 rounded-md bg-blue-600 hover:bg-blue-700 transition-all cursor-pointer" href={'/signup'}>Get started</Link>
-       
-        <Link className="border border-gray-600 p-2 rounded-md  hover:bg-gray-400 hover:text-black transition-all cursor-pointer" href={' https://buymeacoffee.com/oisegiwaokn'}>Buy me a coffee</Link>
+        <Link
+          className="border border-gray-600 p-2 rounded-md bg-blue-800 hover:bg-blue-900 transition-all cursor-pointer"
+          href={"/signup"}
+        >
+          Get started
+        </Link>
+
+        <Link
+          className="border border-blue-600 p-2 rounded-md  hover:bg-blue-600 hover:text-black transition-all cursor-pointer"
+          href={" https://buymeacoffee.com/oisegiwaokn"}
+        >
+          Buy me a coffee
+        </Link>
       </div>
     </div>
-  )
+  );
 }
