577a81b714f52544c9ced5c4aed11e676bde7953
hey-oise <oise.giwa.okugbe@gmail.com>
2026-06-11 08:20:35 +0100

    init

--- app/page.tsx
+++ app/page.tsx
@@ -24,7 +24,7 @@ export default function App() {
           href={" https://buymeacoffee.com/oisegiwaokn"}
           target="_blank"
         >
-          Buy me a coffee ☕
+          buyMeACoffee
         </a>
       </div>
     </div>
