c5857b97935428db1b1379308d3d407ade23eed3
hey-oise <oise.giwa.okugbe@gmail.com>
2026-06-11 16:01:21 +0100

    init

--- app/dashboard/page.jsx
+++ app/dashboard/page.jsx
@@ -9,7 +9,7 @@ if (!user) {
   redirect("/");
 }
 export default function Dashboard() {
-  const [isLoading, setLoading] = useState(false);
+  const [isLoading, setLoading] = useState(true);
   return (
     <div className="bg-gray-800 min-h-screen pl-60">
       <SideBar currentRoute={"dashboard"} />
@@ -20,15 +20,19 @@ export default function Dashboard() {
           <h1 className="capitalize text-2xl font-semibold text-pink-50 m-auto w-fit mt-10">
             dashboard
           </h1>
-          <div className="flex flex-col gap-10 ml-10 mt-20 w-fit">
-            <p className="text-gray-400 font-bold capitalize text-2xl">stats</p>
-            <div className="flex flex-row gap-15">
-              <p className="text-center text-blue-400 capitalize font-semibold">
-                24 <br /> snippets
+          <div>
+            <div className="flex flex-col gap-10 ml-10 mt-20 w-fit">
+              <p className="text-gray-400 font-bold capitalize text-2xl">
+                stats
               </p>
-              <p className="text-center text-green-200 capitalize font-semibold">
-                12 <br /> playgrounds
-              </p>
+              <div className="flex flex-row gap-15">
+                <p className="text-center text-blue-400 capitalize font-semibold">
+                  24 <br /> snippets
+                </p>
+                <p className="text-center text-green-200 capitalize font-semibold">
+                  12 <br /> playgrounds
+                </p>
+              </div>
             </div>
           </div>
         </>
