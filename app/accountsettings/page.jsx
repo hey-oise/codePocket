be082befbe9993a4fc4dd9723fff018092b1a877
hey-oise <oise.giwa.okugbe@gmail.com>
2026-06-13 00:14:08 +0100

    fixed user settings

--- app/accountsettings/page.jsx
+++ app/accountsettings/page.jsx
@@ -1,12 +1,13 @@
+"use client";
 import { AccountSettings } from "@stackframe/stack";
-import { user } from "../user";
+import user from "../user";
 import { redirect } from "next/navigation";
 import Back from "../components/back";
 
-if (!user) {
-  redirect("/");
-}
-export default function Snippets() {
+export default function accountSettings() {
+  if (!user()) {
+    redirect("/");
+  }
   return (
     <div className="bg-gray-900 min-h-screen pl-5 pt-10">
       <Back />
